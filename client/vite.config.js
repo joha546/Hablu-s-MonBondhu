6f72c7b4ab9a660d81423910c4e39f41c71ab731
Md Khaled Bin Joha <joh60483@gmail.com>
2025-12-01 01:37:19 +0600

    feat: add prod grade dockerfile

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   server: {
       host: true,
       port: 5173,
+      allowedHosts: ['*'],
       // proxy /api to backend container (Docker Compose service name: backend)
       proxy: {
         "/api": {
